6c1e753a155112b0ef905bb185d4f9127f39715d
Tomas <tomasboccardo@gmail.com>
2013-09-14 17:53:32 -0300

    Hago andar el display controller

--- tp1/DisplayController.vhd
+++ tp1/DisplayController.vhd
@@ -7,6 +7,7 @@ entity DisplayController is
   port(
 	clk 	: 	in std_logic;
 	enable	: 	in std_logic;
+	rst 	:   in std_logic;
 	bcd0 	:	in std_logic_vector(3 downto 0);
 	bcd1 	:	in std_logic_vector(3 downto 0);
 	bcd2 	:	in std_logic_vector(3 downto 0);
@@ -66,13 +67,12 @@ component DispSelect is
 		q  :   out std_logic_vector(3 downto 0)
 	);
 end component;
-signal rst : std_logic :='0';
-signal gen_en:  std_logic;
-signal count: std_logic_vector(1 downto 0);
-signal selected_in, selected_dsp: std_logic_vector(3 downto 0);
-signal segm_t : std_logic_vector(7 downto 0);
+signal gen_en:  std_logic := '0';
+signal count: std_logic_vector(1 downto 0) := "00";
+signal selected_in, selected_dsp: std_logic_vector(3 downto 0) := "0000";
+signal segm_t : std_logic_vector(7 downto 0) := "00000000";
 begin
-    instGenEnable: 	GenEnable generic map(50000) port map(clk,rst,enable,gen_en);
+	instGenEnable: 	GenEnable generic map(cant) port map(clk,rst,enable,gen_en);
 	instCont2b: 	Cont2b port map(clk,rst,gen_en,count);
 	instMux:		Mux port map(bcd0,bcd1,bcd2,bcd3, count , selected_in);
 	instDispSelect: DispSelect port map(count, selected_dsp);
@@ -96,6 +96,7 @@ component DisplayController is
   port(
 	clk 	: 	in std_logic;
 	enable	: 	in std_logic;
+	rst 	:   in std_logic;
 	bcd0 	:	in std_logic_vector(3 downto 0);
 	bcd1 	:	in std_logic_vector(3 downto 0);
 	bcd2 	:	in std_logic_vector(3 downto 0);
@@ -105,18 +106,19 @@ component DisplayController is
 	segm	: 	out std_logic_vector(7 downto 0)
   );
 end component DisplayController;
- signal ck_t, en: std_logic := '1';
+ signal ck_t, en, rst: std_logic := '1';
  signal bcd0, bcd1, bcd2, bcd3 : std_logic_vector(3 downto 0);
- signal selected_dsp : std_logic_vector(3 downto 0);
- signal segm	: 	std_logic_vector(7 downto 0);
+ signal selected_dsp : std_logic_vector(3 downto 0) := "0000";
+ signal seg	: 	std_logic_vector(7 downto 0):= "00000000";
 begin
 	ck_t <= not ck_t after 10 ns;
+	rst <= '0' after 1 ns;
 	
 	bcd0(3) <= '0';bcd0(2) <= '0';bcd0(1) <= '0';bcd0(0) <= '1';
 	bcd1(3) <= '0';bcd1(2) <= '0';bcd1(1) <= '1';bcd1(0) <= '0';
 	bcd2(3) <= '0';bcd2(2) <= '0';bcd2(1) <= '1';bcd2(0) <= '1';
 	bcd3(3) <= '0';bcd3(2) <= '1';bcd3(1) <= '0';bcd3(0) <= '0';
 	
-	instDC: DisplayController generic map(5) port map(ck_t,en,bcd0,bcd1,bcd2,bcd3,selected_dsp,segm);
+	instDC: DisplayController generic map(5) port map(ck_t,en,rst,bcd0,bcd1,bcd2,bcd3,selected_dsp,seg);
 end; 
 
